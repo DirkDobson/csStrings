d763bf0b9b101296b41c2bcf2f66d80bf997c087
Dirk Dobson <dirkvancouver@live.com>
2018-11-14 19:06:53 -0700

    inial commit

---
+++ csStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
