866bad686963ae97a236255717b82c149ada9c17
Dirk Dobson <dirkvancouver@live.com>
2018-11-14 19:20:35 -0700

    cat string

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Dirk";
+            var lastName = "Dobson";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, LastName);
+
+            var names = new string[3] { "John", "Jane", "James"}
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+
+        }
+    }
+}
